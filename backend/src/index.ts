b841394f898779ae4a6b03702d735081f912e706
Vedanth Aggarwal <vedanth.aggarwal@gmail.com>
2026-05-20 14:37:13 +0400

    routes + control

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,10 @@ import {ENV} from "./config/env"
 import cors from "cors"
 import { clerkMiddleware } from '@clerk/express'
 
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import commentRoutes from "./routes/commentRoutes";
+
 const app = express()
 
 app.use(cors({origin: ENV.FRONTEND_URL}))
@@ -21,5 +25,9 @@ app.get("/", (req, res) => {
     },
   });
 })
+
+app.use("/api/users", userRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/comments", commentRoutes)
 
 app.listen(ENV.PORT, () => console.log('server listening on PORT',ENV.PORT))
