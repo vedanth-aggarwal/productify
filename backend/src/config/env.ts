92acc872efb4276faeb2d6676bff2eb2a6cc5b7a
Vedanth Aggarwal <vedanth.aggarwal@gmail.com>
2026-05-21 14:41:08 +0400

    all-done

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,6 +1,6 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config({quiet: true})
+dotenv.config({ quiet: true });
 
 export const ENV = {
   PORT: process.env.PORT,
@@ -9,4 +9,4 @@ export const ENV = {
   FRONTEND_URL: process.env.FRONTEND_URL,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-}
+};
